d50f5811b8180c1401d373c18e043c008c180200
Yaroslav G <yarmcfly@gmail.com>
2023-06-13 22:13:23 +0300

    version 1.0.

--- CalloriesMe/CalloriesMeApp.swift
+++ CalloriesMe/CalloriesMeApp.swift
@@ -8,10 +8,14 @@
 import SwiftUI
 
 @main
-struct CalloriesMeApp: App {
+struct SampleCoreDataApp: App {
+    @StateObject private var dataController = DataController()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext,
+                              dataController.container.viewContext)
         }
     }
 }
