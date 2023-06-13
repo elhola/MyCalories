daf7056fec2602c6c103db8b1d9e909e9228360a
Yaroslav G <yarmcfly@gmail.com>
2023-06-13 21:17:32 +0300

    Initial Commit

---
+++ CalloriesMe/CalloriesMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalloriesMeApp.swift
+//  CalloriesMe
+//
+//  Created by Yaroslav G on 13.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct CalloriesMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
